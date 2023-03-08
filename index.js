94572ebd77c6b59b4edd36fbe2ca58ceb717676a
Martin Rodriguez <54999192+ChasingFearTv@users.noreply.github.com>
2023-03-07 16:25:09 -0800

    a workLoad lol

---
+++ index.js
@@ -0,0 +1,13 @@
+var drums = document.querySelectorAll(".drum").length;
+for (var i = 0; i < drums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        this
+
+
+    })
+}
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+//         audio.play();
